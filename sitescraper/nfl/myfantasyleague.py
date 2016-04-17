89b7866af01ab2026e7fdd20251a63f78741cca7
jdasi <jeff_dasilva@hotmail.com>
2016-04-17 18:20:26 +0000

    improve scraper

--- sitescraper/nfl/myfantasyleague.py
+++ sitescraper/nfl/myfantasyleague.py
@@ -1,24 +1,44 @@
 
 
 import unittest
+import datetime
 from sitescraper.scraper import SiteScraper
 
 class MyFantasyLeagueDotComScraper(SiteScraper):
 
-    def __init__(self):
+    def __init__(self, id, year=datetime.datetime.now().year):
         super(SiteScraper, self).__init__()
-        self.url = "http://home.myfantasyleague.com"
+
+        self.id = id
+        self.year = year
+        self.url = None
+
+
+    def scrape(self):
+        #url = "http://home.myfantasyleague.com"
+        #url = "http://www56.myfantasyleague.com/" + str(year) + "/home/" + str(id)
+
+        url = "http://www56.myfantasyleague.com/" + str(self.year) + "/options?L=" + str(self.id) + "&O=17"
+        s = SiteScraper(url)
+        s.scrape()
+        self.soup = s.soup
+
 
 class TestMyFantasyLeagueDotComScraper(unittest.TestCase):
 
     def testHomeScrape(self):
-        s = MyFantasyLeagueDotComScraper()
+        s = MyFantasyLeagueDotComScraper(43970,2016)
         s.scrape()
         self.assertNotEquals(s.soup,None)
         #print s.soup
 
     def testOracleLeague(self):
-        #http://www56.myfantasyleague.com/2016/home/43790
+
+        s = MyFantasyLeagueDotComScraper(43790,2016)
+        s.scrape()
+        self.assertNotEquals(s.soup,None)
+        print s.soup
+
         self.assertTrue(True)
 
 if __name__ == '__main__':
