009281253105011006ca8517d7d5f7552b1888ab
jeffdasilva <jeff_dasilva@hotmail.com>
2016-09-03 01:38:58 -0700

    Lots of new commands migrated from top level stattrack.py to command.py

--- cli/cmd/command.py
+++ cli/cmd/command.py
@@ -4,6 +4,7 @@ Created on Sep 2, 2016
 @author: jdasilva
 '''
 from abc import ABCMeta, abstractmethod
+import copy
 import unittest
 
 
@@ -12,17 +13,79 @@ import unittest
 class Command():
     __metaclass__ = ABCMeta
 
+    GenericCommands = []
+
     def __init__(self, name):
-        self.name = None
+        self.name = name
+        self.aliases = []
+        self.hidden = False
 
     @abstractmethod
     def help(self, args, parser): pass
 
-    @abstractmethod
-    def matches(self, cmd): pass
+    def extendedHelp(self, args, parser):
+        return "No further info for command " + self.name
+
+    def getCmdKeyWord(self, cmd):
+        return cmd.split(' ',1)[0].lower()
+
+    def getCmdArgs(self, cmd):
+        cmdKeyWord = self.getCmdKeyWord(cmd)
+
+        cmdHasKeyWord = False
+        if cmdKeyWord == self.name.lower():
+            cmdHasKeyWord = True
+        if not cmdHasKeyWord:
+            for alias in self.aliases:
+                if cmdKeyWord == alias.lower():
+                    cmdHasKeyWord = True
+
+        if cmdHasKeyWord and cmd.lower().startswith(cmdKeyWord):
+            return cmd.lower()[len(cmdKeyWord):].lstrip()
+        else:
+            return cmd
+
+    def matches(self, cmd):
+
+        if self.name == None:
+            return False
+
+        cmdKeyWord = self.getCmdKeyWord(cmd)
+
+        if cmdKeyWord.lower() == self.name.lower():
+            return True
+
+        for alias in self.aliases:
+            if alias.lower() == cmdKeyWord.lower():
+                return True
+
+        return False
+
+    def preApplyMessage(self, cmd, parser):
+        return None
 
     @abstractmethod
     def apply(self, cmd, parser): pass
+
+    def statusTrue(self, parser):
+        from cli.parser import StatTrackParser
+        parser.status = StatTrackParser.StatusTrue
+        return parser.status
+
+    def statusFalse(self, parser):
+        from cli.parser import StatTrackParser
+        parser.status = StatTrackParser.StatusFalse
+        return parser.status
+
+    def statusError(self, parser):
+        from cli.parser import StatTrackParser
+        parser.status = StatTrackParser.StatusError
+        return parser.status
+
+    def statusExit(self, parser):
+        from cli.parser import StatTrackParser
+        parser.status = StatTrackParser.StatusExit
+        return parser.status
 
 ##########################################################
 
@@ -32,45 +95,367 @@ class ErrorCommand(Command):
 
     def __init__(self):
         super(ErrorCommand, self).__init__('error')
+        self.hidden = True
 
     def help(self, args, parser):
-        helpText = '''
-assert an error
-'''
-        return helpText
-
-    def matches(self):
-        return False
+        return "Assert an error"
 
     def apply(self, cmd, parser):
-        from cli.parser import StatTrackParser
         response = "ERROR: " + cmd
-        parser.status = StatTrackParser.StatusError
+        self.statusError(parser)
         return response
+
+Command.GenericCommands.append(ErrorCommand())
+##########################################################
+
+##########################################################
+# blank
+class BlankCommand(Command):
+
+    def __init__(self):
+        super(BlankCommand, self).__init__('blank')
+        self.hidden = True
+        self.aliases.append('')
+
+    def help(self, args, parser):
+        return "Do nothing"
+
+    def apply(self, cmd, parser):
+        self.statusTrue(parser)
+        return ""
+
+Command.GenericCommands.append(BlankCommand())
 ##########################################################
 
 ##########################################################
 # help
 class HelpCommand(Command):
     def __init__(self):
-        super(ErrorCommand, self).__init__('help')
+        super(HelpCommand, self).__init__('help')
 
     def help(self, args, parser):
-        helpText = '''
-displays this help message
-'''
-        return helpText
-
-    def matches(self):
-        return False
+        return "Display this help message"
 
     def apply(self, cmd, parser):
-        from cli.parser import StatTrackParser
-        response = "ERROR: " + cmd
-        parser.status = StatTrackParser.StatusError
+
+        response = "\n" + parser.bold("COMMANDS") + "\n\n"
+        response += '{0: >15}'.format("<command>") + ":  " + "<description>" + "\n\n"
+
+        for cmd in sorted(parser.commands, key=lambda c: c.name):
+            if cmd.hidden:
+                continue
+            response += '{0: >15}'.format(cmd.name) + ":  " + cmd.help(cmd, parser) + "\n"
+
+        response += "\n" + parser.bold("AUTHOR") + "\n\tWritten by Jeff DaSilva.\n"
+
+
+        self.statusTrue(parser)
         return response
 
+Command.GenericCommands.append(HelpCommand())
 ##########################################################
+
+##########################################################
+# undo
+class UndoCommand(Command):
+    def __init__(self):
+        super(UndoCommand, self).__init__('undo')
+
+    def help(self, args, parser):
+        return "Undo the last command"
+
+    def apply(self, cmd, parser):
+
+        if len(parser.undo_stack) == 0:
+            response = parser.error("Nothing to undo")
+        else:
+            parser.undoMode = True
+            cmd = parser.undo_stack.pop()
+            response = parser.processCommand(cmd)
+            parser.undoMode = False
+
+        return response
+
+Command.GenericCommands.append(UndoCommand())
+##########################################################
+
+##########################################################
+# autosave
+class AutoSaveCommand(Command):
+    def __init__(self):
+        super(AutoSaveCommand, self).__init__('autosave')
+
+    def help(self, args, parser):
+        helpText = "Toggle autosave "
+        if parser.autosave:
+            helpText += "off [autosave is currently enabled]"
+        else:
+            helpText += "on [autosave is currently disabled]"
+        return helpText
+
+    def apply(self, cmd, parser):
+
+        parser.autosave = not parser.autosave
+        if parser.autosave:
+            response = "autosave is on"
+        else:
+            response = "autosave is off"
+
+        if not parser.undoMode:
+            parser.undo_stack.append("autosave")
+
+        self.statusTrue(parser)
+        return response
+
+Command.GenericCommands.append(AutoSaveCommand())
+##########################################################
+
+##########################################################
+# pop
+class PopCommand(Command):
+    def __init__(self):
+        super(PopCommand, self).__init__('pop')
+
+    def help(self, args, parser):
+        return "Pop previously pushed saved player database off the stack"
+
+    def apply(self, cmd, parser):
+
+        if len(parser.db_stack) == 0:
+            response = parser.error("Nothing to pop")
+        elif parser.league is None:
+            response = parser.error("Can't perform pop because parser league is not set")
+        else:
+            parser.league.db = parser.db_stack.pop()
+            response = "Popping player database stack to restore old state"
+
+            if not parser.undoMode:
+                parser.undo_stack.append("push")
+
+            self.statusTrue(parser)
+
+        return response
+
+Command.GenericCommands.append(PopCommand())
+##########################################################
+
+##########################################################
+# push
+class PushCommand(Command):
+    def __init__(self):
+        super(PushCommand, self).__init__('push')
+
+    def help(self, args, parser):
+        return "Push current player database to the stack to temporarily save it. Restore it later with pop command"
+
+    def apply(self, cmd, parser):
+
+        if parser.league is None:
+            response = parser.error("Nothing to push. The League is not configured")
+        elif parser.league.db is None:
+            response = parser.error("Nothing to push. The League's database is not configured")
+        else:
+            parser.db_stack.append(copy.deepcopy(parser.league.db))
+            response = "Pushing current player database onto the stack to save current state"
+
+            if not parser.undoMode:
+                parser.undo_stack.append("pop")
+
+            self.statusTrue(parser)
+
+        return response
+
+Command.GenericCommands.append(PushCommand())
+##########################################################
+
+##########################################################
+# print
+class PrintCommand(Command):
+    def __init__(self):
+        super(PrintCommand, self).__init__('print')
+        self.aliases.append("echo")
+
+    def help(self, args, parser):
+        return "Echo a line of text"
+
+    def apply(self, cmd, parser):
+        self.statusTrue(parser)
+        return self.getCmdArgs(cmd)
+
+Command.GenericCommands.append(PrintCommand())
+##########################################################
+
+##########################################################
+# quit
+class QuitCommand(Command):
+    def __init__(self):
+        super(QuitCommand, self).__init__('quit')
+        self.aliases.append("exit")
+        self.aliases.append("q")
+
+    def help(self, args, parser):
+        return "Exit this program"
+
+    def apply(self, cmd, parser):
+        self.statusExit(parser)
+        return "Goodbye Cruel World"
+
+Command.GenericCommands.append(QuitCommand())
+##########################################################
+
+##########################################################
+# update
+class UpdateCommand(Command):
+    def __init__(self):
+        super(UpdateCommand, self).__init__('update')
+
+    def help(self, args, parser):
+        return "Update player database by web scraping applicable web sites"
+
+    def preApplyMessage(self, cmd, parser):
+        return "Updating Player Database from Web. Please Wait..."
+
+    def apply(self, cmd, parser):
+
+        if parser.league is None:
+            response = parser.error("Nothing to update. The League is not configured")
+        elif parser.league.db is None:
+            response = parser.error("Nothing to update. The League's database is not configured")
+        else:
+            pushCmd = PushCommand()
+            pushCmd.apply("push", parser)
+            parser.league.db.wget()
+            response = "Player Database Updated"
+            self.statusTrue(parser)
+
+        return response
+
+Command.GenericCommands.append(UpdateCommand())
+##########################################################
+
+##########################################################
+# save
+class SaveCommand(Command):
+    def __init__(self):
+        super(SaveCommand, self).__init__('save')
+
+    def help(self, args, parser):
+        return "Save player database to disk"
+
+    def preApplyMessage(self, cmd, parser):
+        return "Saving player database to disk..."
+
+    def apply(self, cmd, parser):
+
+        if parser.league is None:
+            response = parser.error("Nothing to save. The League is not configured")
+        elif parser.league.db is None:
+            response = parser.error("Nothing to save. The League's database is not configured")
+        else:
+            parser.league.db.save()
+            response = "Player database saved"
+            self.statusTrue(parser)
+
+            if not parser.undoMode:
+                parser.undo_stack.append("print Can't undo save operation")
+
+        return response
+
+Command.GenericCommands.append(SaveCommand())
+##########################################################
+
+##########################################################
+# load
+class LoadCommand(Command):
+    def __init__(self):
+        super(LoadCommand, self).__init__('load')
+
+    def help(self, args, parser):
+        return "Load player database from disk"
+
+    def preApplyMessage(self, cmd, parser):
+        return "Loading player database from disk..."
+
+    def apply(self, cmd, parser):
+
+        if parser.league is None:
+            response = parser.error("Nothing to load. The League is not configured")
+        elif parser.league.db is None:
+            response = parser.error("Nothing to load. The League's database is not configured")
+        else:
+            pushCmd = PushCommand()
+            pushCmd.apply("push", parser)
+            parser.league.db.load()
+            response = "Player database loaded"
+            self.statusTrue(parser)
+
+        return response
+
+Command.GenericCommands.append(LoadCommand())
+##########################################################
+
+##########################################################
+# list
+class ListCommand(Command):
+    def __init__(self):
+        super(ListCommand, self).__init__('list')
+        self.aliases.append("ls")
+
+    def help(self, args, parser):
+        return "List all players in the player queue"
+
+    def apply(self, cmd, parser):
+
+        if len(parser.player_list) == 0:
+            response = "Player Queue is Empty"
+            self.statusFalse(parser)
+        else:
+            response = "---------------------------------------------------------------------\n"
+            for i, player in enumerate(parser.player_list):
+                if i >= 25:
+                    break
+
+                response += player.name + "\n"
+
+            response += "---------------------------------------------------------------------\n"
+
+            self.statusTrue(parser)
+        return response
+
+Command.GenericCommands.append(ListCommand())
+##########################################################
+
+##########################################################
+# search
+class SearchCommand(Command):
+    def __init__(self):
+        super(SearchCommand, self).__init__('search')
+
+    def help(self, args, parser):
+        return "Search for player"
+
+    def apply(self, cmd, parser):
+
+        if parser.league is None:
+            response = parser.error("Nothing to search. The League is not configured")
+        elif parser.league.db is None:
+            response = parser.error("Nothing to search. The League's database is not configured")
+        else:
+
+            player_list_query = parser.league.db.get(self.getCmdArgs(cmd))
+            if len(player_list_query) == 0:
+                response = "No Players Found for search string: " + self.getCmdArgs(cmd)
+                self.statusFalse(parser)
+            else:
+                parser.player_list = player_list_query
+                response = ListCommand().apply("list", parser)
+
+        return response
+
+Command.GenericCommands.append(SearchCommand())
+##########################################################
+
+
 
 
 class TestCommands(unittest.TestCase):
@@ -86,6 +471,23 @@ class TestCommands(unittest.TestCase):
 
         self.assertNotEqual(c, None)
         self.assertTrue(False, "If you reach here then 'Command' is not an abstract class")
+
+    def testHelp(self):
+        from cli.parser import StatTrackParser
+        p = StatTrackParser(None)
+        p.commands = [ HelpCommand() ]
+        p.processCommandAndResponse("help")
+        self.assertNotEqual(p.status, StatTrackParser.StatusError)
+
+    def testPrint(self):
+        from cli.parser import StatTrackParser
+        pcmd = PrintCommand()
+        self.assertTrue(pcmd.matches("print foo"))
+        self.assertTrue(pcmd.matches("echo foo"))
+        self.assertEquals(pcmd.apply("print foo bar",StatTrackParser(None)),"foo bar")
+        self.assertEquals(pcmd.apply("echo foo bar",StatTrackParser(None)),"foo bar")
+        self.assertEquals(pcmd.apply("foo bar",StatTrackParser(None)),"foo bar")
+        self.assertEquals(pcmd.apply("echo echo foo bar",StatTrackParser(None)),"echo foo bar")
 
 
 
--- cli/parser.py
+++ cli/parser.py
@@ -3,9 +3,10 @@ Created on Sep 1, 2016
 
 @author: jdasilva
 '''
+import os
 import unittest
 
-from cli.cmd.command import ErrorCommand
+from cli.cmd.command import Command, ErrorCommand, SearchCommand
 
 
 class StatTrackParser(object):
@@ -20,11 +21,14 @@ class StatTrackParser(object):
         self.status = StatTrackParser.StatusTrue
 
         self.commands = []
-        self.defaultCommand = None
+        self.commands += Command.GenericCommands
+
+        self.defaultCommand = SearchCommand()
         self.errorCommand = ErrorCommand()
 
         self.player_list = []
         self.undo_stack = []
+        self.undoMode = False
         self.db_stack = []
         self.autosave = True
 
@@ -44,12 +48,16 @@ class StatTrackParser(object):
 
         for commandIter in self.commands:
             if commandIter.matches(cmd):
+
+                if commandIter.preApplyMessage(cmd, self) is not None:
+                    print commandIter.preApplyMessage(cmd, self)
+
                 return commandIter.apply(cmd, self)
 
         if self.defaultCommand is not None:
-            return self.defaultCommand.apply(cmd)
+            return self.defaultCommand.apply(cmd, self)
         else:
-            return self.errorCommand.apply("Invalid Command",self)
+            return self.error("Invalid Command")
 
     def processResponse(self, response, status):
         print response
@@ -60,6 +68,11 @@ class StatTrackParser(object):
         status = self.processResponse(response, self.status)
         return status
 
+    def bold(self,string):
+        return '\033[1m' + string + '\033[0m'
+
+    def error(self,errorMsg):
+        return self.errorCommand.apply(errorMsg,self)
 
 
 class TestStatTrackParser(unittest.TestCase):
@@ -79,12 +92,104 @@ class TestStatTrackParser(unittest.TestCase):
         self.assertEquals(l.name,"jds.1")
         pass
 
-
     def testErrorCommand(self):
         p = StatTrackParser(None)
         status = p.processCommandAndResponse("invalid command")
         self.assertEqual(status, StatTrackParser.StatusError)
         pass
+
+    def testNoLeagueSimpleScript(self):
+        p = StatTrackParser(None)
+
+        script = []
+        script.append(("help",StatTrackParser.StatusTrue))
+        script.append(("",StatTrackParser.StatusTrue))
+        script.append(("blank",StatTrackParser.StatusTrue))
+        script.append(("error this is an error",StatTrackParser.StatusError))
+        script.append(("help",StatTrackParser.StatusTrue))
+        script.append(("autosave",StatTrackParser.StatusTrue))
+        script.append(("help",StatTrackParser.StatusTrue))
+        script.append(("autosave",StatTrackParser.StatusTrue))
+        script.append(("help",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusError))
+        script.append(("pop",StatTrackParser.StatusError))
+        script.append(("push",StatTrackParser.StatusError))
+        script.append(("echo hello world",StatTrackParser.StatusTrue))
+        script.append(("print hello world",StatTrackParser.StatusTrue))
+        script.append(("quit",StatTrackParser.StatusExit))
+        script.append(("exit",StatTrackParser.StatusExit))
+        script.append(("q",StatTrackParser.StatusExit))
+        script.append(("bogus command",StatTrackParser.StatusError))
+        script.append(("undo",StatTrackParser.StatusError))
+        script.append(("update",StatTrackParser.StatusError))
+        script.append(("pop",StatTrackParser.StatusError))
+
+        for cmd_status in script:
+            cmd = cmd_status[0]
+            expectedStatus = cmd_status[1]
+            print "% " + cmd
+            retStatus = p.processCommandAndResponse(cmd)
+            self.assertEqual(retStatus, expectedStatus)
+
+    def testScript(self):
+
+        from db.playerdb import PlayerDB
+        from db.player.player import Player
+        from league.league import League
+
+        pDB = PlayerDB()
+        pDB.saveFile = os.path.dirname(os.path.abspath(__file__)) + "/../data/test_parserplayerdb.pickle"
+        pDB.add(Player("June", "Team-June"))
+        pDB.add(Player("Rudy DaSilva", "Team-Rudy"))
+        pDB.add(Player("Frankie", "Team-Frankie"))
+        league = League(name='JDS',db=pDB)
+        league.parser.defaultCommand = None
+
+        script = []
+        script.append(("help",StatTrackParser.StatusTrue))
+        script.append(("push",StatTrackParser.StatusTrue))
+        script.append(("pop",StatTrackParser.StatusTrue))
+        script.append(("pop",StatTrackParser.StatusError))
+        script.append(("undo",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusError))
+        script.append(("save",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusError))
+        script.append(("load",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusTrue))
+        script.append(("undo",StatTrackParser.StatusError))
+
+        script.append(("bogus command",StatTrackParser.StatusError))
+        script.append(("June",StatTrackParser.StatusError))
+
+        script.append(("search june",StatTrackParser.StatusTrue))
+        script.append(("search bogus command",StatTrackParser.StatusFalse))
+
+        for cmd_status in script:
+            cmd = cmd_status[0]
+            expectedStatus = cmd_status[1]
+            print "% " + cmd
+            retStatus = league.parser.processCommandAndResponse(cmd)
+            self.assertEqual(retStatus, expectedStatus)
+
+        league.parser.defaultCommand = SearchCommand()
+        script = []
+        script.append(("bogus command",StatTrackParser.StatusFalse))
+        script.append(("June",StatTrackParser.StatusTrue))
+        script.append(("rUdY d",StatTrackParser.StatusTrue))
+        script.append(("search u",StatTrackParser.StatusTrue))
+        script.append(("ls",StatTrackParser.StatusTrue))
+
+
+        for cmd_status in script:
+            cmd = cmd_status[0]
+            expectedStatus = cmd_status[1]
+            print "% " + cmd
+            retStatus = league.parser.processCommandAndResponse(cmd)
+            self.assertEqual(retStatus, expectedStatus)
 
 
 if __name__ == "__main__":
